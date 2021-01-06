026092226246a883ca7fd31336c27ee7dc860dc2
Miguel Bogota <miguelbogota.rico@outlook.com>
2021-01-05 21:38:08 -0500

    Removing logger in prod

--- src/core/state/root.store.ts
+++ src/core/state/root.store.ts
@@ -3,6 +3,9 @@ import { logger } from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import { persistedReducer } from 'core/state/root.reducer';
 
-const middlewares = [logger];
+const middlewares = process.env.NODE_ENV !== 'development' ?  [] : [
+  logger,
+];
+
 export const rootStore = createStore(persistedReducer, applyMiddleware(...middlewares));
 export const rootPersistor = persistStore(rootStore);
