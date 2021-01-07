c09a45a720ec4b75fa7eaa6a8ec643a6c819113e
Miguel Bogota <miguelbogota.rico@outlook.com>
2021-01-07 11:42:26 -0500

    Added route for goinf to a category in the shop

--- src/components/collection-preview/collection-preview.component.tsx
+++ src/components/collection-preview/collection-preview.component.tsx
@@ -6,11 +6,14 @@ import {
 } from './collection-preview.styles';
 import { ShopCollection } from 'core/models/collection.model';
 import { CollectionItem } from 'components/collection-item/collection-item.component';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 
-export const CollectionPreview: FC<ShopCollection> = ({ title, items }) => {
+export const CollectionPreview: FC<ShopCollection> = ({ title, items, routeName }) => {
+  const history = useHistory();
+  const match = useRouteMatch();
   return (
     <CollectionPreviewContainer>
-      <TitleContainer>{title.toUpperCase()}</TitleContainer>
+      <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>{title.toUpperCase()}</TitleContainer>
       <PreviewContainer>
         {items
           .filter((_, idx) => idx < 4)
