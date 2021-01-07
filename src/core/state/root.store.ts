dcedb9592e30e55cc09cdb412e33642f6102f0e1
Miguel Bogota <miguelbogota.rico@outlook.com>
2021-01-07 17:22:58 -0500

    Changed how middleware are loaded in the app

--- src/core/state/root.store.ts
+++ src/core/state/root.store.ts
@@ -3,9 +3,15 @@ import { logger } from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import { persistedReducer } from 'core/state/root.reducer';
 
-const middlewares = process.env.NODE_ENV !== 'development' ?  [] : [
-  logger,
-];
+// Production middlewares
+const middlewares = [];
+
+// Development middlewares
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(...[
+    logger,
+  ]);
+}
 
 export const rootStore = createStore(persistedReducer, applyMiddleware(...middlewares));
 export const rootPersistor = persistStore(rootStore);
